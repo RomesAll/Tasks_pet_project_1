dc6e6837844d017290410c5daa81a2220607152c
Roman Beskokotov <roma@gmail.com>
2025-09-24 18:22:16 +0300

    Добавил паттерн repo

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from database import Base, engine, session_fabric
 from model import *
 from sqlalchemy import select, text
+from repository import TasksDAO
+from schemas import TasksAddSchemas, TasksGetSchemas
 import asyncio
 
 async def test():
     async with engine.begin() as conn:
         res = await conn.execute(text("SELECT 'hello'"))
-        print(res.all())
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+        t = TasksDAO()
+        orm1 = await t.get_tasks()
+        orm2 = await t.post_tasks(TasksAddSchemas(task='test', desc='aaa'))
+        orm3 = await t.put_tasks(TasksGetSchemas(id=1, task='test', desc='aaa'))
+        orm4 = await t.delete_tasks(1)
+        print(orm1)
 
 asyncio.run(test())
